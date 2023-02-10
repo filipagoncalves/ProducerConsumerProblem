677f558e5ef52d7d41177322cf87cdec39d2ae99
filipagoncalves <filipa.i.goncalves@gmail.com>
2023-02-10 16:46:25 +0000

    Container update

--- src/Container.java
+++ src/Container.java
@@ -1,13 +1,28 @@
-import java.util.Queue;
+import java.util.*;
 
-public class Container<I> implements Queue {
-    private int container;
+public class Container<I>{
+    private int counter = 0;
+    private int maxCapacity;
+    private Queue<String> queue;
 
-    public Container(int container){
-        this.container = container;
+    public Container(int maxCapacity){
+        this.maxCapacity = maxCapacity;
+        this.queue = new ArrayDeque<>();
     }
 
-    public void add(){
+    public void addDishes(String plateName) throws InterruptedException {
+        if(counter >= 0 && counter < maxCapacity){
+            this.queue.add(plateName);
+            counter++;
+        } else {
+            wait();
+        }
+    }
 
+    public void removeDishes(){
+        if(counter >= 0){
+            this.queue.poll();
+            counter--;
+        }
     }
 }
