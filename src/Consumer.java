298d6af1dad5d4b3089a5733662a49c28fed9c61
nunomartins78 <122387013+nunomartins78@users.noreply.github.com>
2023-02-10 15:11:59 +0000

    Consumer Updated

--- src/Consumer.java
+++ src/Consumer.java
@@ -1,2 +1,11 @@
-public class Consumer {
+public class Consumer implements Runnable{
+
+    private int consumedDishes;
+
+    public Consumer(Container queue, int consumedDishes) {
+    }
+
+    @Override
+    public void run() {
+    }
 }
